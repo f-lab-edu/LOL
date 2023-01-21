b17cefb60e7f9be0d94e01eccacdf8bb6c178db8
spicypunch <miniguni66@naver.com>
2023-01-21 17:26:08 +0900

    commit

--- app/src/main/java/com/example/lol/patter/MainViewModel.kt
+++ app/src/main/java/com/example/lol/patter/MainViewModel.kt
@@ -4,14 +4,21 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.lol.retrofit.LOLResponse.LOLResponseItem
 import com.example.lol.retrofit.LOLService
 import com.example.lol.retrofit.RetrofitConnection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainViewModel(): ViewModel() {
+class MainViewModel() : ViewModel() {
+    private val job = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + job)
 
     private val _items = MutableLiveData<ArrayList<LOLResponseItem>>()
     val items: LiveData<ArrayList<LOLResponseItem>>
@@ -27,27 +34,29 @@ class MainViewModel(): ViewModel() {
     }
 
     fun loadUserInfo() {
-        val retrofitAPI = RetrofitConnection.getInstance().create(LOLService::class.java)
-        retrofitAPI.getInformation(
-            //api 요청이 실패한다면 인증키 유효기간이 지났기 때문(인증키 유효기간 하루)
-            "RGAPI-51ba6f6b-c589-4fc0-8ba6-3c3668761edd"
-        ).enqueue(object : Callback<List<LOLResponseItem>> {
-            override fun onResponse(
-                call: Call<List<LOLResponseItem>>,
-                response: Response<List<LOLResponseItem>>
-            ) {
-                if (response.isSuccessful) {
-                    Log.e("api_test", _items.value.toString())
-                    response.body()?.let { _items.value = it as ArrayList<LOLResponseItem> }
-                } else {
-                    _fail.value = true
+        uiScope.launch {
+            val retrofitAPI = RetrofitConnection.getInstance().create(LOLService::class.java)
+            retrofitAPI.getInformation(
+                //api 요청이 실패한다면 인증키 유효기간이 지났기 때문(인증키 유효기간 하루)
+                "RGAPI-51ba6f6b-c589-4fc0-8ba6-3c3668761edd"
+            ).enqueue(object : Callback<List<LOLResponseItem>> {
+                override fun onResponse(
+                    call: Call<List<LOLResponseItem>>,
+                    response: Response<List<LOLResponseItem>>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.e("api_test", _items.value.toString())
+                        response.body()?.let { _items.value = it as ArrayList<LOLResponseItem> }
+                    } else {
+                        _fail.value = true
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<List<LOLResponseItem>>, t: Throwable) {
-                Log.e("가져오기 실패", "실패?")
-                t.printStackTrace()
-            }
-        })
+                override fun onFailure(call: Call<List<LOLResponseItem>>, t: Throwable) {
+                    Log.e("가져오기 실패", "실패?")
+                    t.printStackTrace()
+                }
+            })
+        }
     }
 }
